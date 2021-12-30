3efbe9b704fe83c861ef501117022ad11844704f
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-12-30 19:59:23 +0300

    add homepage view

--- hood/views.py
+++ hood/views.py
@@ -3,3 +3,37 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html') 
+
+class WebsiteListView(View):
+    def get(self, request):
+        websites = Website.objects.all().order_by('-uploaded_on')
+        form = SiteForm()
+
+        context = {
+            'site_list':websites,
+            'form':form,
+        }
+
+        return render(request, 'website_of_day.html', context) 
+
+    def post(self, request):
+        websites = Website.objects.all().order_by('-uploaded_on')
+        site_of_day = Website.objects.all().order_by('-uploaded_on').first()
+        form = SiteForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            new_site = form.save(commit = False)
+            new_site.author = request.user
+
+            if 'img' in request.FILES:
+                new_site.image = request.FILES['img']
+
+            new_site.save()   
+
+        context = {
+            'site_list':websites,
+            'site_of_day':site_of_day,
+            'form':form,
+        }
+
+        return render(request, 'website_of_day.html', context)      
