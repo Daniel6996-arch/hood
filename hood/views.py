488efea399984bb8484dc833fe01260e240a8fc6
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-12-30 20:02:39 +0300

    add homepage view

--- hood/views.py
+++ hood/views.py
@@ -1,39 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html') 
 
-class WebsiteListView(View):
+class HoodView(View):
     def get(self, request):
-        websites = Website.objects.all().order_by('-uploaded_on')
-        form = SiteForm()
+        hoods = Hood.objects.all().order_by('-uploaded_on')
+        form = HoodForm()
 
         context = {
-            'site_list':websites,
+            'hood_list':hood,
             'form':form,
         }
 
-        return render(request, 'website_of_day.html', context) 
+        return render(request, 'hood.html', context) 
 
     def post(self, request):
-        websites = Website.objects.all().order_by('-uploaded_on')
-        site_of_day = Website.objects.all().order_by('-uploaded_on').first()
-        form = SiteForm(request.POST, request.FILES)
+        hoods = Hood.objects.all().order_by('-uploaded_on')
+        hood_of_day = Hood.objects.all().order_by('-uploaded_on').first()
+        form = HoodForm(request.POST, request.FILES)
 
         if form.is_valid():
-            new_site = form.save(commit = False)
-            new_site.author = request.user
+            new_hood = form.save(commit = False)
+            new_hood.author = request.user
 
             if 'img' in request.FILES:
-                new_site.image = request.FILES['img']
+                new_hood.image = request.FILES['img']
 
-            new_site.save()   
+            new_hood.save()   
 
         context = {
-            'site_list':websites,
-            'site_of_day':site_of_day,
+            'hood_list':hoods,
+            'hood_of_day':hood_of_day,
             'form':form,
         }
 
-        return render(request, 'website_of_day.html', context)      
+        return render(request, 'hood.html', context)      
