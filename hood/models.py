e8b9ef1b2f3b427b344abfc94dc46595bd8ae15e
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-12-31 05:04:23 +0300

    update requirements.txt

--- hood/models.py
+++ hood/models.py
@@ -8,19 +8,19 @@ class NeighbourHood(models.Model):
     hood_name = models.CharField(primary_key = True, max_length = 60)
     admin = models.ForeignKey(User, on_delete=models.CASCADE)
     hood_location = models.CharField(max_length = 120)
-    occupants_count = models.IntergerField()
+    occupants_count = models.IntegerField()
     uploaded_on = models.DateTimeField(auto_now_add=True)
 
 
 class User(models.Model):
     name = models.OneToOneField(User, primary_key = True, verbose_name = 'user', related_name = 'profile', on_delete = models.CASCADE)
-    id = models.IntergerField()
-    hood_id = models.ForeignKey(NeighbourHood, related_name = 'neighbourhood')   
+    id = models.IntegerField()
+    #hood_id = models.ForeignKey(NeighbourHood,primary_key = True, verbose_name = 'user', related_name = 'neighbourhood', on_delete = models.CASCADE)   
     email = models.EmailField()
 
 class Business(models.Model):
     business_name = models.CharField(max_length = 120)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    hood_id = models.ForeignKey(NeighbourHood, related_name = 'neighbourhood')
+    #hood_id = models.ForeignKey(NeighbourHood, related_name = 'neighbourhood')
     business_email = models.EmailField()
     uploaded_on = models.DateTimeField(auto_now_add=True)
