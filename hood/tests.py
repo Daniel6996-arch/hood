6ea9e82cf7e0cb3517ba1de90265c4c8d1ce7ac1
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2022-01-11 11:17:56 +0300

    update tests

--- hood/tests.py
+++ hood/tests.py
@@ -31,3 +31,32 @@ class ProfileTestClass(TestCase):
         self.profile.delete_profile()
         users = User.objects.all()
         self.assertTrue(len(users) > 0)  
+
+
+class HoodTestClass(TestCase):  # Website class test
+    def setUp(self):
+        # creating a user
+        user = User.objects.create(
+            username="test_user", first_name="site", last_name="awards"
+        )
+        
+
+        self.hood = NeighbourHood(
+            hood_admin= user,
+            hood_name = 'nai'
+            hood_location = "Test Site",
+            occupants_count="Description",
+        )
+    def test_instance(self):
+        self.assertTrue(isinstance(self.hood, NeighbourHood))
+
+    def test_save_method(self):
+        self.hood.save_site()
+        hoods = NeighbourHood.objects.all()
+        self.assertTrue(len(hoods) > 0)
+
+    def test_delete_method(self):
+        self.hood.save_site()
+        self.hood.delete_site()
+        hoods = NeighbourHood.objects.all()
+        self.assertTrue(len(hoods) == 0)          
