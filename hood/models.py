d321027d738563badd4578328c27744a4df3aced
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2022-01-11 11:14:00 +0300

    update tests

--- hood/models.py
+++ hood/models.py
@@ -11,12 +11,29 @@ class NeighbourHood(models.Model):
     occupants_count = models.IntegerField()
     uploaded_on = models.DateTimeField(auto_now_add=True)
 
+    def save_hood(self):
+        self.save() 
+
+    def delete_hood(self):
+        self.delete()
+
 
 class UserProfile(models.Model):
     username = models.ForeignKey(User,primary_key = True,verbose_name = 'user', related_name = 'profile', on_delete = models.CASCADE)
     #id = models.IntegerField(primary_key = True, default=0)
     hood = models.ForeignKey(NeighbourHood, on_delete = models.CASCADE, default=0)   
     email = models.EmailField()
+
+    def save_profile(self):
+        self.save() 
+
+    def delete_profile(self):
+        self.delete() 
+
+    @classmethod
+    def search_user(cls,search_term):
+        users = cls.objects.filter(user__username = search_term)
+        return users    
 
 class Business(models.Model):
     business_name = models.CharField(primary_key=True, max_length = 120)
@@ -25,7 +42,19 @@ class Business(models.Model):
     business_email = models.EmailField()
     uploaded_on = models.DateTimeField(auto_now_add=True)
 
+    def save_business(self):
+        self.save() 
+
+    def delete_business(self):
+        self.delete()
+
 class Posts(models.Model):
     author = models.ForeignKey(User, on_delete  = models.CASCADE) 
     body = models.TextField(default= 'some string')  
     created_on =  models.DateTimeField(auto_now_add=True)
+
+    def save_post(self):
+        self.save() 
+
+    def delete_post(self):
+        self.delete()
--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from .models import Posts, Business, NeighbourHood, UserProfile
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        
+        user = User.objects.create(
+            username="test_user", first_name="dan", last_name="dante"
+        )
+        neighbourhood = NeighbourHood.objects.create(
+            hood_name='hood', hood_admin=user, hood_location='location', occupants_count='333'
+        )
+
+        self.profile = UserProfile(
+            username = user,
+            hood=neighbourhood,
+            email = "image@jpg",
+            #followers = "followers.set()"
+        )
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, UserProfile))
+
+    def test_save_method(self):
+        self.profile.save_profile()
+        users = User.objects.all()
+        self.assertTrue(len(users) > 0)
+
+    def test_delete_method(self):
+        self.profile.save_profile()
+        self.profile.delete_profile()
+        users = User.objects.all()
+        self.assertTrue(len(users) > 0)  
