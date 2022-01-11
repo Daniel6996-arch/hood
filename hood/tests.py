95b5acf5d34c9a7f6e76c6e14879e117489ea88f
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2022-01-11 11:29:28 +0300

    update tests

--- hood/tests.py
+++ hood/tests.py
@@ -73,7 +73,7 @@ class BusinessTestClass(TestCase):
 
         self.business = Business(
             business_name = 'user',
-            business_user = user
+            business_user = user,
             hood=neighbourhood,
             business_email = "image@jpg",
             #followers = "followers.set()"
@@ -90,4 +90,33 @@ class BusinessTestClass(TestCase):
         self.business.save_business()
         self.business.delete_business()
         businesses = Business.objects.all()
-        self.assertTrue(len(businesses) > 0)         
+        self.assertTrue(len(businesses) == 0)        
+
+class PostTestClass(TestCase):
+    def setUp(self):
+        
+        user = User.objects.create(
+            username="test_user", first_name="dan", last_name="dante"
+        )
+        neighbourhood = NeighbourHood.objects.create(
+            hood_name='hood', hood_admin=user, hood_location='location', occupants_count='333'
+        )
+
+        self.post = Posts(
+            author = user,
+            body = 'body',
+            
+        )
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Posts))
+
+    def test_save_method(self):
+        self.post.save_post()
+        posts = Posts.objects.all()
+        self.assertTrue(len(posts) > 0)
+
+    def test_delete_method(self):
+        self.post.save_post()
+        self.post.delete_post()
+        posts = Posts.objects.all()
+        self.assertTrue(len(posts) == 0)                  
