1db3b62c1458c154acf0eaf1ede736eb2ca7cb89
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2022-01-11 09:39:17 +0300

    update database info

--- hood/models.py
+++ hood/models.py
@@ -5,22 +5,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 # Create your models here.
 class NeighbourHood(models.Model):
-    hood_name = models.CharField(primary_key = True, max_length = 60)
-    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    hood_name = models.CharField( max_length = 60)
+    hood_admin = models.ForeignKey(User,primary_key = True, on_delete=models.CASCADE)
     hood_location = models.CharField(max_length = 120)
     occupants_count = models.IntegerField()
     uploaded_on = models.DateTimeField(auto_now_add=True)
 
 
 class UserProfile(models.Model):
-    username = models.ForeignKey(User, primary_key=True, verbose_name = 'user', related_name = 'profile', on_delete = models.CASCADE)
+    username = models.ForeignKey(User,verbose_name = 'user', related_name = 'profile', on_delete = models.CASCADE)
     #id = models.IntegerField(primary_key = True, default=0)
     hood = models.ForeignKey(NeighbourHood, on_delete = models.CASCADE, default=0)   
     email = models.EmailField()
 
 class Business(models.Model):
-    business_name = models.CharField(max_length = 120, primary_key = True)
-    username  = models.ForeignKey(User, on_delete = models.CASCADE)
+    business_name = models.CharField(primary_key=True, max_length = 120)
+    business_user  = models.ForeignKey(User, on_delete = models.CASCADE)
     hood = models.ForeignKey(NeighbourHood, related_name = 'neighbourhood', on_delete = models.CASCADE, default=0)
     business_email = models.EmailField()
     uploaded_on = models.DateTimeField(auto_now_add=True)
--- neighourhood/settings.py
+++ neighourhood/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'neighourhood.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'yy',
+        'NAME': 'yyt',
         'USER': 'moringa',
         'PASSWORD': 'Access',
         'HOST': 'localhost',
